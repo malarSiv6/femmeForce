230ad0b23bba68b4fec7d4ce70ad0e586cfc2c55
Malar Sivakumar <malaraustin1@gmail.com>
2023-07-18 14:31:29 -0500

    Initial Commit

---
+++ femmeForce/femmeForceApp.swift
@@ -0,0 +1,17 @@
+//
+//  femmeForceApp.swift
+//  femmeForce
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct femmeForceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
