a6b0a0613e6d3ee0694858a362e5f9d66f28a131
Malar Sivakumar <malaraustin1@gmail.com>
2023-07-19 10:41:53 -0500

    Base

--- femmeForce/ContentView.swift
+++ femmeForce/ContentView.swift
@@ -18,17 +18,18 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
-            Button(/*@START_MENU_TOKEN@*/"Submit Name"/*@END_MENU_TOKEN@*/) {
+            Button(/*@START_MENU_TOKEN@*/"    Submit Name    "/*@END_MENU_TOKEN@*/) {
                 titleText = "Welcome, \(name)!"
             }
             .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
-            .tint(.purple)
+            .tint(Color(red: 0.285, green: 0.184, blue: 0.064))
+            .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.capsule/*@END_MENU_TOKEN@*/)
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
         }
         .padding()
         .frame(width: 500.0, height: 800.0)
         .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 0.991, green: 0.829, blue: 0.75)/*@END_MENU_TOKEN@*/)
-        
-    }
+            }
 }
 
 struct ContentView_Previews: PreviewProvider {
